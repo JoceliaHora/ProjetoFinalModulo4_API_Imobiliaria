5b09e54ee6e61899b2c63a8929ae3851c5adc729
ElianeCristina <eliane_cristinanunescorreia@yahoo.com.br>
2022-05-04 14:37:12 -0300

    entidade clientes

---
+++ src/controllers/cliente-controller.js
@@ -0,0 +1,103 @@
+// Cliente - CRUD
+const Cliente = require('../modells/cliente-model');
+const ClienteDAO = require('../DAO/cliente-dao');
+
+const cliente = (app, bd)=>{
+    const DAOCliente = new ClienteDAO(bd);
+    // Create do CRUD - POST - INSERE REGISTROS NO BANCO:
+    app.post('/cliente', (req, res) => {
+        // armazenando os dados no banco:
+        const body = req.body;
+        const ClienteDado = new Cliente(body.nome, body.cpf, body.telefone, body.endereco, body.tipoDeImovel, body.condicaoDoImovel, body.email, body.senha)
+        const data = async() => {
+            try{
+                const clientes = await DAOCliente.inserirClientes(ClienteDado)
+                res.send(clientes)
+            }catch(err){
+                res.send(err);
+            }
+        }
+        data()
+    })
+
+    // Read do CRUD - READ - EXIBE REGISTROS
+    app.get('/cliente', (req, res) => {
+        const data = async() => {
+            try{
+                const clientes = await DAOCliente.listarCliente();
+                res.send(clientes)
+            }catch(err){
+                res.send(err);
+            }
+        }
+        data()
+    })
+
+    // READ - EXIBE REGISTROS POR PARAMETROS
+    app.get('/cliente/:id', (req, res) => {
+        const id = req.params.id;
+        const data = async() => {
+            try{
+                const clientes = await DAOCliente.listarClienteID(req.params.id);
+                res.send(clientes)
+                console.log(clientes)
+            }catch(err){
+                res.send(err);
+                console.log(err)
+            }
+        }
+        data()
+    })
+
+    // Update do CRUD - UPDATE - ATUALIZA REGISTROS
+    app.put('/cliente/:id', (req, res) => {
+        const body = req.body;
+        const id = req.params.id;
+        const Parametros =[body.nome, body.cpf, body.telefone, body.endereco, body.tipoDeImovel, body.condicaoDoImovel, body.email, body.senha, id]
+        const data = async() => {
+            try{
+                const clientes = await DAOCliente.alterarClientes(req.params.id);
+                res.send(clientes)
+            }catch(err){
+                res.send(err);
+            }
+        }
+        data()
+    })
+
+    // Update com patch - para selecionar qual atributo vai alterar:
+    app.patch('/cliente/:id', (req, res) => {
+        const body = req.body;
+        const id = req.params.id;
+        const data = async() => {
+            try{
+                const clienteDado = await DAOCliente.listarClienteID(id);
+                const ClienteDado = new Cliente(body.nome || clienteDado[0].NOME, body.cpf || clienteDado[0].CPF, body.telefone || clienteDado[0].TELEFONE, 
+                    body.endereco || clienteDado[0].ENDERECO, body.tipoDeImovel || clienteDado[0].TIPODEIMOVEL, body.condicaoDoImovel || clienteDado[0].CONDICAODOIMOVEL, 
+                    body.email || clienteDado[0].EMAIL, body.senha || clienteDado[0].SENHA)
+                const Parametros =[ClienteDado.nome, ClienteDado.cpf, ClienteDado.telefone, ClienteDado.endereco, ClienteDado.tipoDeImovel, 
+                        ClienteDado.condicaoDoImovel, ClienteDado.email, ClienteDado.senha, id]
+                const clientes = await DAOCliente.alterarClientes(Parametros)
+                res.send(clientes)
+            }catch(err){
+                res.send(err)
+            }
+        }
+        data()
+    })
+
+     // Delete do CRUD - DELETAR - DELETA REGISTROS
+    app.delete('/cliente/:id', (req, res) => {
+        const data = async() => {
+            try{
+                const clientes = await DAOCliente.deletarClientes(req.params.id)
+                res.send(clientes)
+            }catch(err){
+                res.send(err)
+            }
+        }
+        data()
+    })
+}
+
+module.exports = cliente;
